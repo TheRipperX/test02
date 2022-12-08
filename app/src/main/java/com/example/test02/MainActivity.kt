f3ec4e7c33903d24eb19eae9ae5279356f1fb6bf
Sx@2663949131zA <Ripper.CS-RET@Hotmail.com>
2022-12-08 16:43:05 +0330

    add toast

--- app/src/main/java/com/example/test02/MainActivity.kt
+++ app/src/main/java/com/example/test02/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.test02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
     }
 
     private fun main() {
-
+        Toast.makeText(this, "add git github", Toast.LENGTH_SHORT).show()
     }
 }
